75d29330e3f77a213d32faed5bdb9f8259efddf9
Paris Tuzun <paristuzun@gmail.com>
2018-07-25 00:21:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+
+function increaseRankBy(n) {
+  var rank = document.querySelectorAll('ul.ranked-list li');
+ 
+for (var i = 0; i < rank.length; i++) {
+  rank[i].innerHTML = parseInt(rank[i].innerHTML) + n;
+}
+}
+
+function deepestChild() {
+  var deepest = document.getElementById('grand-node').querySelectorAll('div')
+  return deepest[deepest.length-1]
+}
