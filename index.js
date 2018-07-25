949f48acff4e47398ffa6a5d56817e331567387d
Paris Tuzun <paristuzun@gmail.com>
2018-07-25 00:11:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function increaseRankBy(n) {
   var rank = document.querySelectorAll('ul.ranked-list li');
  
 for (var i = 0; i < rank.length; i++) {
-  rank[i].innerHTML = parseInt(rank[i] + rank[i].innerHTML);
+  rank[i].innerHTML = parseInt(rank[i].innerHTML);
 }
 return n
 }
